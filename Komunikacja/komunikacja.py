8fc3c4c03d5283ba6dd86e0f69f970117998e4f4
PatrykPrzekwas <rzepka94@gmail.com>
2016-03-19 21:15:37 +0100

    Creates server responsible for receiving commmands via external sources

--- Komunikacja/komunikacja.py
+++ Komunikacja/komunikacja.py
@@ -9,11 +9,12 @@ class TCPCommunication(object):
     Sockets are managed in two threads running alternately.
     Data is validate and parse before sending.
     """
-    def __init__(self, receiver=0, sender=0):
+    def __init__(self, receiver=0, sender=0, ip_addr='localhost'):
         # TODO change to PC ip_addr
         self.tcp_ip = 'localhost'
         self.tcp_sender_port = int(sender)
         self.tcp_receiver_port = int(receiver)
+        self.ip_addr = ip_addr
         self.buffer_size = 1024
 
     def start_threads(self):
@@ -31,7 +32,7 @@ class TCPCommunication(object):
     def _sender(self):
         _ = raw_input("Press enter after connecting...")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.tcp_ip, self.tcp_sender_port))
+        s.connect((self.ip_addr, self.tcp_sender_port))
 
         # setting connection
         s.send('?')
@@ -74,9 +75,10 @@ class TCPCommunication(object):
         conn.close()
 
 
-if __name__ == "__main__":
-    # TODO add ip_addr here for PC addr
-    receiver_port = raw_input('Receiver port: ')
-    sender_port = raw_input('Sender port: ')
-    connection = TCPCommunication(receiver_port, sender_port)
-    connection.start_threads()
+# if __name__ == "__main__":
+#     # TODO add ip_addr here for PC addr
+#     receiver_port = raw_input('Receiver port: ')
+#     sender_port = raw_input('Sender port: ')
+#     ip_addr = raw_input('Sender port: ')
+#     connection = TCPCommunication(receiver_port, sender_port, ip_addr)
+#     connection.start_threads()

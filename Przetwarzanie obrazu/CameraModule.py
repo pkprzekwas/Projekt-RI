ae4c4b9976683e9ca13160599a568c7d6bb9277a
miczyg1395 <miczyg@hotmail.com>
2016-06-08 15:07:56 +0200

    Poprawka - obraz
    Zwiększona elastyczność algorytmu i odsiew niechcianych lini

--- Przetwarzanie obrazu/CameraModule.py
+++ Przetwarzanie obrazu/CameraModule.py
@@ -52,43 +52,57 @@ class cameraModule:
                         for self.frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
                                 self.img = self.frame.array
                                 img=self.img
-                                img2 = img
                                 A = []
                                 B = []
                                 C = []
                                 D = []
+                                height, width, _ = img.shape
+                                img = img[height*0.55:height, 20:width-20]
+                                img2 = img
                                 img = cv.Sobel(img, -1, 1, 0)
-                                img = cv.Canny(img, 100, 200, apertureSize=3)
-
-                                linie = cv.HoughLines(img, 1, np.pi / 25, 35)
+                                img = cv.Canny(img, 60, 120, apertureSize=3)
+                                punkty = []
+                                linie = cv.HoughLines(img, 1, np.pi / 20, 50)
                                 if linie is not None:
-                                    for i in range(0, len(linie)):
-                                        for rho, theta in linie[i]:
-                                            a = np.cos(theta)
-                                            b = np.sin(theta)
-                                            x0 = a * rho
-                                            y0 = b * rho
-                                            x1 = int(x0 + 1000 * (-b))
-                                            y1 = int(y0 + 1000 * (a))
-                                            x2 = int(x0 - 1000 * (-b))
-                                            y2 = int(y0 - 1000 * (a))
-                                            theta = float(theta)
-                                            if (theta >= 0.0 and theta <= 1.1):
-                                                    A = [x1,y1]
-                                                    B = [x2,y2]                                              
-                                            elif (theta >2.6 and theta < 3.1):
-                                                    C = [x1,y1]
-                                                    D = [x2,y2]
-                                            if A != [] and B!=[] and C != [] and D != [] :
-                                                    center = self.line_intersection([A, B], [C, D])
-                                                    if 150 <= center[0] <= 170 :                                                            
-                                                            cv.line(img2, (A[0], A[1]), (B[0], B[1]), (0, 0, 255), 2)
-                                                            cv.line(img2, (C[0], C[1]), (D[0], D[1]), (0, 0, 255), 2)
-                                                            cv.circle(img2, center , 5 , (255,0,0), thickness = -1)
+                                        for i in range(0, len(linie)):
+                                                for rho, theta in linie[i]:
+                                                        a = np.cos(theta)
+                                                        b = np.sin(theta)
+                                                        x0 = a * rho
+                                                        y0 = b * rho
+                                                        x1 = int(x0 + 1000 * (-b))
+                                                        y1 = int(y0 + 1000 * a)
+                                                        x2 = int(x0 - 1000 * (-b))
+                                                        y2 = int(y0 - 1000 * a)
+                                                        theta = float(theta)
+                                                        if 0.0 <= theta < 1.3:
+                                                                A = (x1, y1)
+                                                                B = (x2, y2)
+                                                                punkty.append([A, B])
+                                                        elif 2.0 < theta < 3.14:
+                                                                C = (x1, y1)
+                                                                D = (x2, y2)
+                                                                punkty.append([C, D])
 
-                                                
+                                suma = 0
+                                n = 0
+
+                                for x in xrange(0, punkty.__len__()):
+                                        cv.line(img2, punkty[x][0], punkty[x][1], (0, 0, 255), 2)
+                                        for y in xrange(x, punkty.__len__()):
+                                                center = self.line_intersection(punkty[x], punkty[y])
+                                                if center:
+                                                        if 0 <= center[0] <= width:
+                                                                suma += center[0]
+                                                                n += 1
+                                                                cv.circle(img2, center, 5, (255, 0, 0), thickness=-1)
+
+                                h, w, _ = img2.shape
+                                if n != 0:
+                                        avg = suma/n
+                                        cv.rectangle(img2, (avg-5, h/2-5), (avg+5, h/2+5), color=(0, 255, 0), thickness=2)
+
                                 cv.imshow('obraz', img2)
-
                                 #cv2.imshow('img',self.img)
                                 key = cv2.waitKey(10) & 0xFF
                                 self.rawCapture.truncate(0)
